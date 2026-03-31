688999221a8f95ad6f137a7f35b3df3a9d296bc0
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-31 17:55:33 +0300

    Add `BookingRepository` interface extending JpaRepository for `Booking` entity

---
+++ src/main/java/com/bookingsystem/booking_service/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.bookingsystem.booking_service.repository;
+
+import com.bookingsystem.booking_service.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, String> {
+}
