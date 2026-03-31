4c47056b14eccd72eaadc15ead547ac1594ff75d
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-31 19:00:13 +0300

    Annotate `Property` entity with Lombok annotations and JPA `@Entity` and `@Table` annotations

--- src/main/java/com/bookingsystem/booking_service/entity/Property.java
+++ src/main/java/com/bookingsystem/booking_service/entity/Property.java
@@ -1,11 +1,18 @@
 package com.bookingsystem.booking_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@Table(name = "properties")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Property {
 
     @Id
